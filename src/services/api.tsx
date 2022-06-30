d3c607a9f75313e99e772cb2c4c4f0c0e5597fdc
Mateus Antonio Franceschina <mateus.franceschina@pampa.compasso>
2022-06-30 19:47:51 -0300

    add routes and zod

--- src/services/api.tsx
+++ src/services/api.tsx
@@ -1,8 +1,28 @@
+import {
+  AuthenticateUser,
+  BuyRequest,
+  BuyTicket,
+  CreateUser,
+  FormCreateEvent,
+  Paginate
+} from "@types";
 import axios from "axios";
 
 const baseUrl = "http://localhost:8080/";
 
 export const api = {
-  createUser: (body) => axios.post(baseUrl + "user/create", body),
-  authenticateUser: (body) => axios.post(baseUrl + "user/authenticate", body)
+  createUser: (body: CreateUser) => axios.post(baseUrl + "user/create", body),
+  authenticateUser: (body: AuthenticateUser) =>
+    axios.post(baseUrl + "user/authenticate", body),
+  findAllEvents: (params: Paginate) =>
+    axios.get(baseUrl + "event/", { params }),
+  createEvent: (body: FormCreateEvent) =>
+    axios.post(baseUrl + "event/create", body),
+  findAllTicketsByEvent: (eventId) => axios.get(baseUrl + "ticket/" + eventId),
+  findTicketTypesByEventId: (eventId: string) =>
+    axios.get(baseUrl + "ticket/types/" + eventId),
+  checkTicketAvailability: (eventId: string, body: BuyTicket[]) =>
+    axios.post(baseUrl + "ticket/availability/" + eventId, body),
+  buyTickets: (eventId: string, body: BuyRequest) =>
+    axios.post(baseUrl + "ticket/buy" + eventId, body)
 };
